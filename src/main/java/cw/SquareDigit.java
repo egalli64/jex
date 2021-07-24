39c6c3d980d25a9c33de2dea21c910200dd3363a
Emanuele Galli <egalli64@gmail.com>
2021-07-24 11:25:35 +0200

    Codewars Digit Square, two variations

---
+++ src/main/java/cw/SquareDigit.java
@@ -0,0 +1,44 @@
+package cw;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.stream.Collectors;
+
+/**
+ * Square every digit of a number and concatenate them
+ * For example: 9119 -> 811181
+ * <p>
+ * https://www.codewars.com/kata/546e2562b03326a88e000020/java
+ */
+public class SquareDigit {
+    public int squareDigitsStack(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Only non negative arguments are accepted!");
+        }
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        while (n > 0) {
+            stack.push(n % 10);
+            n /= 10;
+        }
+
+        StringBuilder result = new StringBuilder();
+        while (!stack.isEmpty()) {
+            int value = stack.pop();
+            result.append(value * value);
+        }
+        return result.length() > 0 ? Integer.parseInt(result.toString()) : 0;
+    }
+
+    public int squareDigits(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Only non negative arguments are accepted!");
+        }
+
+        String result = Integer.toString(n).chars()
+                .map(i -> i - '0').map(i -> i * i)
+                .mapToObj(Integer::toString).collect(Collectors.joining());
+
+        return Integer.parseInt(result);
+    }
+}
---
+++ src/test/java/cw/SquareDigitTest.java
@@ -0,0 +1,48 @@
+package cw;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SquareDigitTest {
+    @Test
+    void squareDigitsStackPlain() {
+        int actual = new SquareDigit().squareDigitsStack(123);
+        int expected = 149;
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+    @Test
+    void squareDigitsStackGiven1() {
+        int actual = new SquareDigit().squareDigitsStack(9119);
+        int expected = 811181;
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void squareDigitsStackGiven2() {
+        int actual = new SquareDigit().squareDigitsStack(0);
+        int expected = 0;
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void squareDigitsPlain() {
+        int actual = new SquareDigit().squareDigits(123);
+        int expected = 149;
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+    @Test
+    void squareDigitsGiven1() {
+        int actual = new SquareDigit().squareDigits(9119);
+        int expected = 811181;
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void squareDigitsGiven2() {
+        int actual = new SquareDigit().squareDigits(0);
+        int expected = 0;
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+}
