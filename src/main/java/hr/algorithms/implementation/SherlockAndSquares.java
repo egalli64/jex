96e93c13514c0ea50b06f537ddb9fde256f375e4
Emanuele <egalli64@gmail.com>
2022-06-05 16:40:52 +0200

    Hackerrank Sherlock And Squares

---
+++ src/main/java/hr/algorithms/implementation/SherlockAndSquares.java
@@ -0,0 +1,13 @@
+/*
+    Prepare > Algorithms > Implementation > Sherlock and Squares
+    https://www.hackerrank.com/challenges/sherlock-and-squares/problem
+
+    Determine the number of square integers within the passed inclusive range
+ */
+package hr.algorithms.implementation;
+
+public class SherlockAndSquares {
+    public static int squares(int a, int b) {
+        return 1 + (int) Math.sqrt(b) - (int) Math.ceil(Math.sqrt(a));
+    }
+}
