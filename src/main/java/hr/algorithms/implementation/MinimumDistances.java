9cfd28f3d7312627ccf098b5e5ea19c32a782fbe
Emanuele <egalli64@gmail.com>
2022-06-27 16:46:50 +0200

    Minimum Distances

---
+++ src/main/java/hr/algorithms/implementation/MinimumDistances.java
@@ -0,0 +1,26 @@
+/*
+    Prepare > Algorithms > Implementation > Minimum Distances
+    https://www.hackerrank.com/challenges/minimum-distances/problem
+
+    Minimum Distance between elements having the same value
+ */
+package hr.algorithms.implementation;
+
+import java.util.*;
+
+public class MinimumDistances {
+    public static int minimumDistances(List<Integer> a) {
+        int result = Integer.MAX_VALUE;
+        Map<Integer, Integer> lastIndices = new HashMap<>();
+        for (int i = 0; i < a.size(); i++) {
+            Integer current = a.get(i);
+            Integer prevIndex = lastIndices.putIfAbsent(current, i);
+            if(prevIndex != null) {
+                result = Math.min(result, i - prevIndex);
+                lastIndices.put(current, i);
+            }
+        }
+
+        return result == Integer.MAX_VALUE ? -1 : result;
+    }
+}
