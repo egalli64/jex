02dddad68f5dad07f79c061af70e9f259bd0549f
Emanuele <egalli64@gmail.com>
2022-07-04 19:02:19 +0200

    repeatEnd ,prefixAgain, and sameStarChar

--- src/main/java/cb/String2.java
+++ src/main/java/cb/String2.java
@@ -6,6 +6,9 @@ package cb;
 import java.util.regex.Pattern;
 
 public class String2 {
+    private String2() {
+    }
+
     /**
      * codingbat.com/prob/p165312
      *
@@ -48,8 +51,10 @@ public class String2 {
     /**
      * codingbat.com/prob/p123614
      *
-     * <p>Java 9 is currently _not_ supported by CodingBat
-     * <p>For a pre-9 solution see {@linkplain String2#countCode(String)}
+     * <p>
+     * Java 9 is currently _not_ supported by CodingBat
+     * <p>
+     * For a pre-9 solution see {@linkplain String2#countCode(String)}
      *
      * @param str a string
      * @return how many times the pattern "co.e" is present in the string
@@ -66,5 +71,57 @@ public class String2 {
      */
     public static boolean bobThere(String str) {
         return str.matches(".*b.b.*");
+    }
+
+    /**
+     * Java 11 not supported by CodingBat, see
+     * {@linkplain String2#repeatEnd(String, int)}
+     */
+    public static String repeatEnd11(String str, int n) {
+        return str.substring(str.length() - n).repeat(n);
+    }
+
+    /**
+     * codingbat.com/prob/p152339
+     * 
+     * @param str a possibly empty string
+     * @param n   a number in [0, string.length()]
+     * @return The last n char of str repeated n times
+     */
+    public static String repeatEnd(String str, int n) {
+        String base = str.substring(str.length() - n);
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            result.append(base);
+        }
+
+        return result.toString();
+    }
+
+    /**
+     * codingbat.com/prob/p136417
+     * 
+     * @param str a non emtpy string
+     * @param n   an int in [1,str.length()]
+     * @return true if the prefix till n is repeated in the string
+     */
+    public static boolean prefixAgain(String str, int n) {
+        String prefix = str.substring(0, n);
+        return str.indexOf(prefix, n) != -1;
+    }
+
+    /**
+     * codingbat.com/prob/p194491
+     * 
+     * @param str a string
+     * @return true if any * in it not in extreme positions has same neighbors
+     */
+    public static boolean sameStarChar(String str) {
+        for (int i = 1; i < str.length() - 1; i++) {
+            if (str.charAt(i) == '*' && str.charAt(i - 1) != str.charAt(i + 1)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- src/test/java/cb/String2Test.java
+++ src/test/java/cb/String2Test.java
@@ -7,26 +7,50 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class String2Test {
     @ParameterizedTest
-    @CsvSource({"The,TThhee", "AAbb,AAAAbbbb", "Hi-There,HHii--TThheerree"})
+    @CsvSource({ "The,TThhee", "AAbb,AAAAbbbb", "Hi-There,HHii--TThheerree" })
     void doubleCharExamples(String input, String expected) {
         assertThat(String2.doubleChar(input)).isEqualTo(expected);
     }
 
     @ParameterizedTest
-    @CsvSource({"aaacodebbb,1", "codexxcode,2", "cozexxcope,2"})
+    @CsvSource({ "aaacodebbb,1", "codexxcode,2", "cozexxcope,2" })
     void countCodeExamples(String input, int expected) {
         assertThat(String2.countCode(input)).isEqualTo(expected);
     }
 
     @ParameterizedTest
-    @CsvSource({"aaacodebbb,1", "codexxcode,2", "cozexxcope,2"})
+    @CsvSource({ "aaacodebbb,1", "codexxcode,2", "cozexxcope,2" })
     void countCode9Examples(String input, int expected) {
         assertThat(String2.countCode9(input)).isEqualTo(expected);
     }
 
     @ParameterizedTest
-    @CsvSource({"abcbob,true", "b9b,true", "bac,false"})
+    @CsvSource({ "abcbob,true", "b9b,true", "bac,false" })
     void bobThereExamples(String input, boolean expected) {
         assertThat(String2.bobThere(input)).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource({ "Hello,3,llollollo", "Hello,2,lolo", "Hello,1,o" })
+    void repeatEnd11Examples(String s, int n, String expected) {
+        assertThat(String2.repeatEnd11(s, n)).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource({ "Hello,3,llollollo", "Hello,2,lolo", "Hello,1,o" })
+    void repeatEndExamples(String s, int n, String expected) {
+        assertThat(String2.repeatEnd(s, n)).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource({ "abXYabc,1,true", "abXYabc,2,true", "abXYabc,3,false" })
+    void prefixAgainExamples(String s, int n, boolean expected) {
+        assertThat(String2.prefixAgain(s, n)).isEqualTo(expected);
+    }
+    
+    @ParameterizedTest
+    @CsvSource({ "xy*yzz,true", "xy*zzz,false", "*xa*az,true" })
+    void sameStarCharExamples(String input, boolean expected) {
+        assertThat(String2.sameStarChar(input)).isEqualTo(expected);
     }
 }
