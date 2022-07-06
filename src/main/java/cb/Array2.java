40cd576f95fb1737d2a8aa2bba89e661887b067f
Emanuele <egalli64@gmail.com>
2022-07-06 21:53:11 +0200

    fizzArray, no14, and matchUp

--- src/main/java/cb/Array2.java
+++ src/main/java/cb/Array2.java
@@ -47,12 +47,65 @@ public class Array2 {
      * @return true if nums does not contain 1 or 3
      */
     public static boolean lucky13(int[] nums) {
-        for(int num : nums) {
-            if(num == 1 || num == 3) {
+        for (int num : nums) {
+            if (num == 1 || num == 3) {
                 return false;
             }
         }
 
         return true;
+    }
+
+    /**
+     * codingbat.com/prob/p180920
+     *
+     * @param n a non-negative integer
+     * @return an array sized n with the natural (zero-based) sequence
+     */
+    public static int[] fizzArray(int n) {
+        int[] result = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            result[i] = i;
+        }
+
+        return result;
+    }
+
+    /**
+     * codingbat.com/prob/p136648
+     *
+     * @param nums an array of int
+     * @return true if no 1, 4 in it
+     */
+    public static boolean no14(int[] nums) {
+        int count1 = 0;
+        int count4 = 0;
+        for (int num : nums) {
+            if (num == 1) {
+                count1 += 1;
+            } else if (num == 4) {
+                count4 += 1;
+            }
+        }
+        return count1 == 0 || count4 == 0;
+    }
+
+    /**
+     * codingbat.com/prob/p136254
+     *
+     * @param nums1 an array
+     * @param nums2 another array, same size
+     * @return times that the elements in the same position differ by 2 or less, but are not equal
+     */
+    public static int matchUp(int[] nums1, int[] nums2) {
+        int result = 0;
+        for (int i = 0; i < nums1.length; i++) {
+            int difference = Math.abs(nums1[i] - nums2[i]);
+            if (difference > 0 && difference < 3) {
+                result += 1;
+            }
+        }
+        return result;
     }
 }
--- src/test/java/cb/Array2Test.java
+++ src/test/java/cb/Array2Test.java
@@ -71,4 +71,26 @@ class Array2Test {
         int[] input = {1, 2, 4};
         assertThat(Array2.lucky13(input)).isFalse();
     }
+
+    @Test
+    void fizzArrayExample1() {
+        int input = 4;
+        int[] expected = {0, 1, 2, 3};
+        assertThat(Array2.fizzArray(input)).containsExactly(expected);
+    }
+
+    @Test
+    void no14Example1() {
+        int[] input = {1, 2, 3};
+        assertThat(Array2.no14(input)).isTrue();
+    }
+
+    @Test
+    void matchUpExample1() {
+        int[] left = {1, 2, 3};
+        int[] right = {2, 3, 10};
+        int expected = 2;
+        assertThat(Array2.matchUp(left, right)).isEqualTo(expected);
+    }
+
 }
