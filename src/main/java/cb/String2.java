da8543c6a6f69af67d9829ccbb0f684128c9b5b7
Emanuele <egalli64@gmail.com>
2022-07-30 20:37:04 +0200

    String-2 done

--- src/main/java/cb/String2.java
+++ src/main/java/cb/String2.java
@@ -344,4 +344,93 @@ public class String2 {
 
         return result.toString();
     }
+
+    /**
+     * codingbat.com/prob/p139564
+     * 
+     * @param str a string
+     * @return remove '*' and preceding/following non-star char
+     */
+    public static String starOut(String str) {
+        if (str.isEmpty()) {
+            return str;
+        } else if (str.length() == 1) {
+            return str.charAt(0) == '*' ? "" : str;
+        }
+
+        StringBuilder result = new StringBuilder();
+        char first = str.charAt(0);
+        char second = str.charAt(1);
+        if (first != '*' && second != '*') {
+            result.append(first);
+        }
+
+        for (int i = 1; i < str.length() - 1; i++) {
+            char prev = str.charAt(i - 1);
+            char cur = str.charAt(i);
+            char next = str.charAt(i + 1);
+            if (!(prev == '*' || cur == '*' || next == '*')) {
+                result.append(cur);
+            }
+        }
+
+        char prev = str.charAt(str.length() - 2);
+        char last = str.charAt(str.length() - 1);
+        if (prev != '*' && last != '*') {
+            result.append(last);
+        }
+
+        return result.toString();
+    }
+
+    /**
+     * codingbat.com/prob/p170829
+     * 
+     * @param str  a string
+     * @param word a non-empty string
+     * @return keep the words, replace other chars with '+'
+     */
+    public static String plusOut(String str, String word) {
+        StringBuilder result = new StringBuilder();
+
+        int cur = 0;
+
+        do {
+            int next = str.indexOf(word, cur);
+            for (int end = next == -1 ? str.length() : next; cur < end; cur++) {
+                result.append('+');
+            }
+            if (next != -1) {
+                result.append(word);
+                cur += word.length();
+            }
+        } while (cur < str.length());
+
+        return result.toString();
+    }
+
+    /**
+     * codingbat.com/prob/p147538
+     * 
+     * @param str  a string
+     * @param word a non-empty string
+     * @return the chars before/after words in str
+     */
+    public static String wordEnds(String str, String word) {
+        StringBuilder result = new StringBuilder();
+
+        int i = 0;
+        while ((i = str.indexOf(word, i)) != -1) {
+            if (i > 0) {
+                result.append(str.charAt(i - 1));
+            }
+
+            i += word.length();
+            if (i < str.length()) {
+                result.append(str.charAt(i));
+            }
+        }
+
+        return result.toString();
+    }
 }
--- src/test/java/cb/String2Test.java
+++ src/test/java/cb/String2Test.java
@@ -125,4 +125,22 @@ class String2Test {
     void zipZapExamples(String s, String expected) {
         assertThat(String2.zipZap(s)).isEqualTo(expected);
     }
+
+    @ParameterizedTest
+    @CsvSource({ "ab*cd,ad", "ab**cd,ad", "sm*eilly,silly" })
+    void starOutExamples(String s, String expected) {
+        assertThat(String2.starOut(s)).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource({ "12xy34,xy,++xy++", "12xy34,1,1+++++", "12xy34xyabcxy,xy,++xy++xy+++xy" })
+    void plusOutExamples(String s, String w, String expected) {
+        assertThat(String2.plusOut(s, w)).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource({ "abcXY123XYijk,XY,c13i", "XY123XY,XY,13", "XY1XY,XY,11" })
+    void wordEndsExamples(String s, String w, String expected) {
+        assertThat(String2.wordEnds(s, w)).isEqualTo(expected);
+    }
 }
