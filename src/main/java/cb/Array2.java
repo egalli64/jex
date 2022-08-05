27a137ee1f580b0590a7d3f51db2a4d7a8ece22e
Emanuele <egalli64@gmail.com>
2022-08-05 08:53:35 +0200

    Array-2 completed

--- src/main/java/cb/Array2.java
+++ src/main/java/cb/Array2.java
@@ -574,4 +574,84 @@ public class Array2 {
 
         return result;
     }
+
+    /**
+     * codingbat.com/prob/p196976
+     * 
+     * @param nums an array
+     * @return 10s go to 0 and to the right
+     */
+    public static int[] withoutTen(int[] nums) {
+        int[] result = new int[nums.length];
+        int j = 0;
+        for (int num : nums) {
+            if (num != 10) {
+                result[j] = num;
+                j += 1;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * codingbat.com/prob/p187050
+     * 
+     * @param nums an array
+     * @return zero replaced by the largest odd value to the right (if any)
+     */
+    public static int[] zeroMax(int[] nums) {
+        int changer = 0;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (nums[i] == 0) {
+                nums[i] = changer;
+            } else if (nums[i] > changer && nums[i] % 2 == 1) {
+                changer = nums[i];
+            }
+        }
+
+        return nums;
+    }
+
+    /**
+     * codingbat.com/prob/p105771
+     * 
+     * @param nums an array
+     * @return even numbers before odd ones
+     */
+    public static int[] evenOdd(int[] nums) {
+        int[] result = new int[nums.length];
+        int even = 0;
+        int odd = result.length - 1;
+        for (int num : nums) {
+            result[num % 2 == 0 ? even++ : odd--] = num;
+        }
+        return result;
+    }
+
+    /**
+     * codingbat.com/prob/p153059
+     * 
+     * @param start first value
+     * @param end   last, not included, value
+     * @return an array with "Fizz" as 3*x, "Buzz" as 5*x, "FizzBuzz" as 3*5*x
+     */
+    public static String[] fizzBuzz(int start, int end) {
+        String[] result = new String[end - start];
+        for (int i = 0, j = start; j < end; i++, j++) {
+            if (j % 3 != 0 && j % 5 != 0) {
+                result[i] = String.valueOf(j);
+                continue;
+            }
+
+            StringBuilder cur = new StringBuilder();
+            if (j % 3 == 0) {
+                cur.append("Fizz");
+            }
+            if (j % 5 == 0) {
+                cur.append("Buzz");
+            }
+            result[i] = cur.toString();
+        }
+        return result;
+    }
 }
--- src/test/java/cb/Array2Test.java
+++ src/test/java/cb/Array2Test.java
@@ -258,4 +258,34 @@ class Array2Test {
         int[] expected = { 0, 0, 1, 1 };
         assertThat(Array2.zeroFront(input)).isEqualTo(expected);
     }
+
+    @Test
+    void withoutTenExample1() {
+        int[] input = { 1, 10, 10, 2 };
+        int[] expected = { 1, 2, 0, 0 };
+        assertThat(Array2.withoutTen(input)).isEqualTo(expected);
+    }
+
+    @Test
+    void zeroMaxExample1() {
+        int[] input = { 0, 5, 0, 3 };
+        int[] expected = { 5, 5, 3, 3 };
+        assertThat(Array2.zeroMax(input)).isEqualTo(expected);
+    }
+
+    @Test
+    void evenOddExample1() {
+        int[] input = { 1, 0, 1, 0, 0, 1, 1 };
+        int[] expected = { 0, 0, 0, 1, 1, 1, 1 };
+        assertThat(Array2.evenOdd(input)).isEqualTo(expected);
+    }
+
+    @Test
+    void fizzBuzzExample1() {
+        int begin = 1;
+        int end = 6;
+        String[] expected = { "1", "2", "Fizz", "4", "Buzz" };
+        assertThat(Array2.fizzBuzz(begin, end)).isEqualTo(expected);
+    }
+
 }
