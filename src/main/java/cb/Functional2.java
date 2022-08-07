38762b8e6087a505fe27094044a3d9965e360bf1
Emanuele <egalli64@gmail.com>
2022-08-07 22:09:36 +0200

    Functional-2 done

--- src/main/java/cb/Functional2.java
+++ src/main/java/cb/Functional2.java
@@ -36,4 +36,74 @@ public class Functional2 {
     public static List<String> noYY(List<String> strings) {
         return strings.stream().map(x -> x + "y").filter(x -> !x.contains("yy")).collect(Collectors.toList());
     }
+
+    /**
+     * codingbat.com/prob/p124510
+     *
+     * @param nums a list of non-negative integers
+     * @return discard numbers endings by 9 (base 10)
+     */
+    public static List<Integer> no9(List<Integer> nums) {
+        nums.removeIf(x -> x % 10 == 9);
+        return nums;
+    }
+
+    /**
+     * codingbat.com/prob/p137274
+     *
+     * @param nums a list
+     * @return no values in [13..19] allowed
+     */
+    public static List<Integer> noTeen(List<Integer> nums) {
+        nums.removeIf(x -> x > 12 && x < 20);
+        return nums;
+    }
+
+    /**
+     * codingbat.com/prob/p194496
+     *
+     * @param strings a list
+     * @return no string with length 4+ allowed
+     */
+    public static List<String> noLong(List<String> strings) {
+        strings.removeIf(x -> x.length() > 3);
+        return strings;
+    }
+
+    /**
+     * codingbat.com/prob/p184496
+     *
+     * @param strings a list
+     * @return no string sized 3 or 4 allowed
+     */
+    public static List<String> no34(List<String> strings) {
+        strings.removeIf(x-> {
+            final int size = x.length();
+            return size > 2 && size < 5;
+        });
+        return strings;
+    }
+
+    /**
+     * codingbat.com/prob/p148198
+     *
+     * @param nums non-negatives
+     * @return doubled, but discarded if in base-10 it ends by 2
+     */
+    public static List<Integer> two2(List<Integer> nums) {
+        return nums.stream().map(x -> x*2).filter(x -> x % 10 != 2).collect(Collectors.toList());
+    }
+
+    /**
+     * codingbat.com/prob/p132748
+     *
+     * @param nums a list
+     * @return squared plus 10, discard if ends in 5 or 6 (base 10)
+     */
+    public static List<Integer> square56(List<Integer> nums) {
+        return nums.stream().map(x -> x * x + 10).filter(x -> {
+            int last = x % 10;
+            return last < 5 || last > 6;
+        }).collect(Collectors.toList());
+    }
 }
--- src/test/java/cb/Functional2Test.java
+++ src/test/java/cb/Functional2Test.java
@@ -3,6 +3,7 @@ package cb;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Functional2Test {
@@ -30,4 +31,51 @@ class Functional2Test {
         Assertions.assertThat(Functional2.noYY(input)).containsExactlyElementsOf(expected);
     }
 
+    @Test
+    void no9Example1() {
+        List<Integer> input = new ArrayList<>(List.of(1, 2, 19));
+        List<Integer> expected = List.of(1, 2);
+
+        Assertions.assertThat(Functional2.no9(input)).containsExactlyElementsOf(expected);
+    }
+
+    @Test
+    void noTeenExample1() {
+        List<Integer> input = new ArrayList<>(List.of(12, 13, 19, 20));
+        List<Integer> expected = List.of(12, 20);
+
+        Assertions.assertThat(Functional2.noTeen(input)).containsExactlyElementsOf(expected);
+    }
+
+    @Test
+    void noLongExample1() {
+        List<String> input = new ArrayList<>(List.of("this", "not", "too", "long"));
+        List<String> expected = List.of("not", "too");
+
+        Assertions.assertThat(Functional2.noLong(input)).containsExactlyElementsOf(expected);
+    }
+
+    @Test
+    void no34Example1() {
+        List<String> input = new ArrayList<>(List.of("a", "bb", "ccc"));
+        List<String> expected = List.of("a", "bb");
+
+        Assertions.assertThat(Functional2.no34(input)).containsExactlyElementsOf(expected);
+    }
+
+    @Test
+    void two2Example1() {
+        List<Integer> input = new ArrayList<>(List.of(1, 2, 3));
+        List<Integer> expected = List.of(4, 6);
+
+        Assertions.assertThat(Functional2.two2(input)).containsExactlyElementsOf(expected);
+    }
+
+    @Test
+    void square56Example1() {
+        List<Integer> input = new ArrayList<>(List.of(3, 1, 4));
+        List<Integer> expected = List.of(19, 11);
+
+        Assertions.assertThat(Functional2.square56(input)).containsExactlyElementsOf(expected);
+    }
 }
