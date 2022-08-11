c776656345dd603cf8f7be9f5997f7bc7dec75e7
Emanuele <egalli64@gmail.com>
2022-08-11 22:21:04 +0200

    Lisa's Workbook

---
+++ src/test/java/hr/algorithms/implementation/LisaSWorkbookTest.java
@@ -0,0 +1,17 @@
+package hr.algorithms.implementation;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LisaSWorkbookTest {
+
+    @Test
+    void workbookSample0() {
+        int actual = LisaSWorkbook.workbook(5, 3, List.of(4, 2, 6, 1, 10));
+        assertThat(actual).isEqualTo(4);
+    }
+}
