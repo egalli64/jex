2744fa1c6e2949c6271dbff89904a46702429b27
Emanuele <egalli64@gmail.com>
2022-08-12 11:18:03 +0200

    minor cleanup

--- src/test/java/hr/algorithms/implementation/LisaSWorkbookTest.java
+++ src/test/java/hr/algorithms/implementation/LisaSWorkbookTest.java
@@ -4,11 +4,9 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LisaSWorkbookTest {
-
     @Test
     void workbookSample0() {
         int actual = LisaSWorkbook.workbook(5, 3, List.of(4, 2, 6, 1, 10));
