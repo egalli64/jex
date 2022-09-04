de4ec2ac6afbb63c3603f78b5392788eacf42bdf
Emanuele <egalli64@gmail.com>
2022-09-04 22:32:45 +0200

    Anagram

---
+++ src/test/java/hr/algorithms/strings/AnagramTest.java
@@ -0,0 +1,14 @@
+package hr.algorithms.strings;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class AnagramTest {
+    @ParameterizedTest
+    @CsvSource({"aaabbb,3", "ab,1", "abc,-1", "mnop,2", "xyyx,0", "xaxbbbxx,1"})
+    void anagramSamples(String input, int expected) {
+        int actual = Anagram.anagram(input);
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+}
