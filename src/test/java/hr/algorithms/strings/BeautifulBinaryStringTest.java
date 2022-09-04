53307ecdd91fba61f245c18a21fbcae1e148677f
Emanuele <egalli64@gmail.com>
2022-09-04 20:47:14 +0200

    Beautiful Binary String

---
+++ src/test/java/hr/algorithms/strings/BeautifulBinaryStringTest.java
@@ -0,0 +1,14 @@
+package hr.algorithms.strings;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class BeautifulBinaryStringTest {
+    @ParameterizedTest
+    @CsvSource({"0101010,2", "01100,0", "0100101010,3"})
+    void beautifulBinaryStringSamples(String input, int expected) {
+        int actual = BeautifulBinaryString.beautifulBinaryString(input);
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+}
