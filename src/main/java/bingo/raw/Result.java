ad76e736c9b487105fa32e41b10a749e94ddd9c6
Emanuele <egalli64@gmail.com>
2022-09-17 20:06:40 +0200

    boolean is not enough

---
+++ src/main/java/bingo/raw/Result.java
@@ -0,0 +1,14 @@
+/*
+    Raw Bingo - A simple CLI app emulating a bingo game
+    Only minimal Java features used
+
+    Hosted on https://github.com/egalli64/jex
+ */
+package bingo.raw;
+
+/**
+ * Extraction effect on a card
+ */
+public enum Result {
+    MISSED, FOUND, WIN
+}
