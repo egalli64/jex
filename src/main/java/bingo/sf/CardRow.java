c2157dc61d75da361cbd420828a44d16f6a68618
Emanuele <egalli64@gmail.com>
2022-09-27 15:49:21 +0200

    First step refactoring

---
+++ src/main/java/bingo/sf/CardRow.java
@@ -0,0 +1,63 @@
+/*
+    SF Bingo - A simple CLI app emulating a bingo game
+
+    Hosted on https://github.com/egalli64/jex
+ */
+package bingo.sf;
+
+import java.util.Arrays;
+
+/**
+ * A single row in a bingo card
+ */
+public class CardRow {
+    /**
+     * the sorted numbers
+     */
+    private final int[] numbers;
+    /**
+     * found matches
+     */
+    private int extracted;
+
+    /**
+     * Ctor
+     *
+     * @param numbers the current row
+     */
+    public CardRow(int[] numbers) {
+        Arrays.sort(numbers);
+        this.numbers = numbers;
+        this.extracted = 0;
+    }
+
+    /**
+     * Is the current number in this row?
+     *
+     * @param current the extracted number
+     * @return the adjusted hit count, or 0 for a miss
+     */
+    public int check(int current) {
+        for (int number : numbers) {
+            if (number == current) {
+                extracted += 1;
+                return extracted;
+            }
+        }
+        return 0;
+    }
+
+    /**
+     * Hit count for the row
+     *
+     * @return [0 .. size]
+     */
+    public int getExtracted() {
+        return extracted;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(numbers);
+    }
+}
