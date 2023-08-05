3014e290ed47d4a12bca2fd25d76e401d38d7814
Emanuele <egalli64@gmail.com>
2023-08-05 21:27:51 +0200

    more tests

--- src/test/java/com/codingbat/recursion1/Array6Test.java
+++ src/test/java/com/codingbat/recursion1/Array6Test.java
@@ -7,12 +7,24 @@ package com.codingbat.recursion1;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class Array6Test {
-    @Test
-    void array6Example1() {
-        int[] input = {1, 6, 4};
-        assertThat(Array6.array6(input, 0)).isTrue();
+    static Stream<Arguments> provider() {
+        return Stream.of( //
+                Arguments.of(new int[] { 1, 6, 4 }, true), //
+                Arguments.of(new int[] { 1, 4 }, false), //
+                Arguments.of(new int[] { 6 }, true) //
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provider")
+    void array11Example1(int[] input, boolean expected) {
+        assertThat(Array6.array6(input, 0)).isEqualTo(expected);
     }
 }
