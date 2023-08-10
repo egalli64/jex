95c992c42b65be39e47bbad9f03d1c8ce752d4d2
Emanuele <egalli64@gmail.com>
2023-08-10 12:28:14 +0200

    readability

--- src/main/java/com/codingbat/logic1/TeaParty.java
+++ src/main/java/com/codingbat/logic1/TeaParty.java
@@ -23,11 +23,10 @@ public class TeaParty {
     public static int solution(int tea, int candy) {
         if (tea < 5 || candy < 5) {
             return 0;
-        }
-        if (tea >= candy * 2 || candy >= tea * 2) {
+        } else if (tea >= candy * 2 || candy >= tea * 2) {
             return 2;
+        } else {
+            return 1;
         }
-
-        return 1;
     }
 }
