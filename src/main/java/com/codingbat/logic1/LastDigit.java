581dea4ffe86841e4efe4bc0f8dd8854901a658c
Emanuele <egalli64@gmail.com>
2023-08-18 16:54:53 +0200

    readability

--- src/main/java/com/codingbat/logic1/LastDigit.java
+++ src/main/java/com/codingbat/logic1/LastDigit.java
@@ -1,7 +1,7 @@
 /*
   CodingBat Java Logic 1 - https://codingbat.com/java/Logic-1
 
-  My repo - https://github.com/egalli64/jex/
+  My repository - https://github.com/egalli64/jex/
  */
 package com.codingbat.logic1;
 
@@ -20,6 +20,21 @@ public class LastDigit {
      * @return true if at least two values have the same last decimal digit
      */
     public static boolean solution(int a, int b, int c) {
+        int ma = a % 10;
+        int mb = b % 10;
+        int mc = c % 10;
+        return ma == mb || mb == mc || mc == ma;
+    }
+
+    /**
+     * Same, but in a more compact way
+     * 
+     * @param a first value
+     * @param b second value
+     * @param c third value
+     * @return true if at least two values have the same last decimal digit
+     */
+    public static boolean compact(int a, int b, int c) {
         a %= 10;
         b %= 10;
         c %= 10;
--- src/test/java/com/codingbat/logic1/LastDigitTest.java
+++ src/test/java/com/codingbat/logic1/LastDigitTest.java
@@ -1,7 +1,7 @@
 /*
   CodingBat Java Logic 1 - https://codingbat.com/java/Logic-1
 
-  My repo - https://github.com/egalli64/jex/
+  My repository - https://github.com/egalli64/jex/
  */
 package com.codingbat.logic1;
 
@@ -15,5 +15,11 @@ class LastDigitTest {
     @CsvSource({ "23, 19, 13, true", "23, 19, 12, false", "23, 19, 3, true" })
     void solutionExamples(int a, int b, int c, boolean expected) {
         assertThat(LastDigit.solution(a, b, c)).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource({ "23, 19, 13, true", "23, 19, 12, false", "23, 19, 3, true" })
+    void compactExamples(int a, int b, int c, boolean expected) {
+        assertThat(LastDigit.compact(a, b, c)).isEqualTo(expected);
     }
 }
