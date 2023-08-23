4c2d6dbe0ec83a8202ab4186f76f5e2ab82c1ee2
Emanuele <egalli64@gmail.com>
2023-08-23 15:38:14 +0200

    minor improvement

--- src/main/java/com/codingbat/string2/CountHi.java
+++ src/main/java/com/codingbat/string2/CountHi.java
@@ -38,6 +38,7 @@ public class CountHi {
         for (int i = 0; i < str.length() - 1; i++) {
             if (str.charAt(i) == 'h' && str.charAt(i + 1) == 'i') {
                 count += 1;
+                i += 1;
             }
         }
 
