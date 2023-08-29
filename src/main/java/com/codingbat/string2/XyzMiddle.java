ebc564725593febc28768015708f9d628cffbb7e
Emanuele <egalli64@gmail.com>
2023-08-29 11:53:08 +0200

    check by size solution

--- src/main/java/com/codingbat/string2/XyzMiddle.java
+++ src/main/java/com/codingbat/string2/XyzMiddle.java
@@ -13,14 +13,48 @@ package com.codingbat.string2;
  */
 public class XyzMiddle {
     /**
-     * Ensure that the string is long enough to contain "xyz", if so, get the index
-     * of its instance close to the center. If "xyz" is there, compare the size of
-     * head and tail.
-     *
+     * Ensure the string is long enough to contain the target. If it has an even
+     * size, the target can't be exactly centered, check if it is more to the left.
+     * Otherwise check if it the target starts from the center minus one. In case of
+     * odd-sized input, this means that it is exactly centered, for a even-sized
+     * input it means it is a one step more to the right.
+     * <p>
+     * The check for "xyz" is delegated to
+     * {@linkplain XyzMiddle#checkXyz(String, int)}
+     * 
      * @param str a string
      * @return true if accepted
      */
     public static boolean solution(String str) {
+        if (str.length() < 3) {
+            return false;
+        } else if (str.length() % 2 == 0 && checkXyz(str, str.length() / 2 - 2)) {
+            return true;
+        } else {
+            return checkXyz(str, str.length() / 2 - 1);
+        }
+    }
+
+    /**
+     * Check if "xyz" is a substring of s from i
+     * 
+     * @param s a string
+     * @param i an index in s
+     * @return true if matches
+     * @throws StringIndexOutOfBoundsException for "bad" i
+     */
+    static boolean checkXyz(String s, int i) {
+        return s.charAt(i) == 'x' && s.charAt(i + 1) == 'y' && s.charAt(i + 2) == 'z';
+    }
+
+    /**
+     * Get the index of its instance close to the center. If "xyz" is there, compare
+     * the size of head and tail.
+     *
+     * @param str a string
+     * @return true if accepted
+     */
+    public static boolean byIndex(String str) {
         int head = str.indexOf("xyz", str.length() / 2 - 3);
         if (head == -1) {
             return false;
--- src/test/java/com/codingbat/string2/XyzMiddleTest.java
+++ src/test/java/com/codingbat/string2/XyzMiddleTest.java
@@ -6,7 +6,9 @@
 package com.codingbat.string2;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -15,5 +17,23 @@ class XyzMiddleTest {
     @CsvSource({ "AAxyzBB,true", "AxyzBB,true", "AxyzBBB,false", "AAAxyzB,false" })
     void solutionExamples(String input, boolean expected) {
         assertThat(XyzMiddle.solution(input)).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource({ "AAxyzBB,true", "AxyzBB,true", "AxyzBBB,false", "AAAxyzB,false" })
+    void byIndexExamples(String input, boolean expected) {
+        assertThat(XyzMiddle.byIndex(input)).isEqualTo(expected);
+    }
+
+    @Test
+    void checkXyzOutOfBound() {
+        assertThatExceptionOfType(StringIndexOutOfBoundsException.class) //
+                .isThrownBy(() -> XyzMiddle.checkXyz("xyz", 42));
+    }
+
+    @ParameterizedTest
+    @CsvSource({ "AAxyzBB,2,true", "AxyzBB,1,true", "AxyzBBB,0,false", "AAAxyzB,4,false" })
+    void checkXyzExamples(String input, int index, boolean expected) {
+        assertThat(XyzMiddle.checkXyz(input, index)).isEqualTo(expected);
     }
 }
