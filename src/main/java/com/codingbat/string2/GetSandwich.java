150276871122c8e70e43e3567977ca3c866dcd3b
Emanuele <egalli64@gmail.com>
2023-08-30 11:11:08 +0200

    no magic

--- src/main/java/com/codingbat/string2/GetSandwich.java
+++ src/main/java/com/codingbat/string2/GetSandwich.java
@@ -12,6 +12,8 @@ package com.codingbat.string2;
  * appearance of "bread", or an empty string
  */
 public class GetSandwich {
+    private static final String DELIM = "bread";
+
     /**
      * Get the starting index of the first and last occurrence of "bread", then
      * generate the substring delimited by them
@@ -20,8 +22,8 @@ public class GetSandwich {
      * @return the extracted substring
      */
     public static String solution(String str) {
-        final int first = str.indexOf("bread");
-        final int last = str.lastIndexOf("bread");
-        return first == last ? "" : str.substring(first + 5, last);
+        final int first = str.indexOf(DELIM);
+        final int last = str.lastIndexOf(DELIM);
+        return first == last ? "" : str.substring(first + DELIM.length(), last);
     }
 }
