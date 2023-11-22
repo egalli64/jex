f3a29af49d6270426b53794eb1991a82e5bbafb5
Emanuele <egalli64@gmail.com>
2023-11-22 09:56:45 +0100

    unhappy ending test added

--- src/test/java/com/codingbat/string3/GHappyTest.java
+++ src/test/java/com/codingbat/string3/GHappyTest.java
@@ -7,6 +7,7 @@ package com.codingbat.string3;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -28,11 +29,15 @@ class GHappyTest {
     void matchExamples(String input, boolean expected) {
         assertThat(GHappy.match(input)).isEqualTo(expected);
     }
-    
 
     @ParameterizedTest
     @CsvSource({ "g,false", "gx,false", "xg,false", "gg,true" })
     void matchShort(String input, boolean expected) {
         assertThat(GHappy.match(input)).isEqualTo(expected);
+    }
+
+    @Test
+    void matchUnhappyEnding() {
+        assertThat(GHappy.match("xxgggxyg")).isFalse();
     }
 }
