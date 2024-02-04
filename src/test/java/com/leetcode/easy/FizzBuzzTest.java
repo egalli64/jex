61a761ea6020c0b9dee6f56f25d0c1e7a6f8d208
Emanuele <egalli64@gmail.com>
2024-02-04 19:39:13 +0100

    fizzBuzz tests

---
+++ src/test/java/com/leetcode/easy/FizzBuzzTest.java
@@ -0,0 +1,34 @@
+/*
+ * https://leetcode.com/problems/fizz-buzz/
+ * 
+ * My repository with solutions - https://github.com/egalli64/jex/
+ */
+package com.leetcode.easy;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class FizzBuzzTest {
+    private static final FizzBuzz instance = new FizzBuzz();
+
+    private static final Stream<Arguments> provider() {
+        return Stream.of( //
+                Arguments.of(3, List.of("1", "2", "Fizz")), //
+                Arguments.of(5, List.of("1", "2", "Fizz", "4", "Buzz")), //
+                Arguments.of(15, List.of("1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz",
+                        "13", "14", "FizzBuzz")) //
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provider")
+    void fizzBuzzExamples(int n, List<String> expected) {
+        List<String> actual = instance.fizzBuzz(n);
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+}
