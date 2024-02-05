31791b0aef2cd94de6a4555188723520623b5378
Emanuele <egalli64@gmail.com>
2024-02-05 22:16:28 +0100

    Roman to Integer

---
+++ src/main/java/com/leetcode/easy/RomanToInteger.java
@@ -0,0 +1,38 @@
+/*
+ * LeetCode 13. Roman to Integer
+ * https://leetcode.com/problems/roman-to-integer/description/
+ * 
+ * My repository with solutions
+ * https://github.com/egalli64/jex/
+ */
+package com.leetcode.easy;
+
+import java.util.Map;
+
+public class RomanToInteger {
+    private static final Map<Character, Integer> mapper = Map.of( //
+            'I', 1, 'V', 5, 'X', 10, 'L', 50, 'C', 100, 'D', 500, 'M', 1000);
+
+    /**
+     * Convert a Roman number to its integer representation
+     * 
+     * @param s a Roman number
+     * @return its integer representation
+     */
+    public int romanToInt(String s) {
+        int result = 0;
+
+        int prev = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int cur = mapper.get(s.charAt(i));
+            if (cur < prev) {
+                result -= cur;
+            } else {
+                result += cur;
+            }
+            prev = cur;
+        }
+
+        return result;
+    }
+}
---
+++ src/test/java/com/leetcode/easy/RomanToIntegerTest.java
@@ -0,0 +1,24 @@
+/*
+ * LeetCode 13. Roman to Integer
+ * https://leetcode.com/problems/roman-to-integer/description/
+ * 
+ * My repository with solutions
+ * https://github.com/egalli64/jex/
+ */
+package com.leetcode.easy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class RomanToIntegerTest {
+    private static final RomanToInteger instance = new RomanToInteger();
+
+    @ParameterizedTest
+    @CsvSource({ "III,3", "LVIII,58", "MCMXCIV,1994" })
+    void romanToIntExamples(String input, int expected) {
+        int actual = instance.romanToInt(input);
+        assertThat(actual).isEqualTo(expected);
+    }
+}
