e583c7e4418ec031283e26129907a10b6e9a9824
Emanuele <egalli64@gmail.com>
2024-02-06 08:54:02 +0100

    Longest Common Prefix

---
+++ src/main/java/com/leetcode/easy/LongestCommonPrefix.java
@@ -0,0 +1,43 @@
+/*
+ * LeetCode 14. Longest Common Prefix
+ * https://leetcode.com/problems/longest-common-prefix/description/
+ * 
+ * My repository with solutions
+ * https://github.com/egalli64/jex/
+ */
+package com.leetcode.easy;
+
+import java.util.Arrays;
+
+/**
+ * Constraints:
+ * 
+ * <li>1 <= strs.length <= 200
+ * <li>0 <= strs[i].length <= 200
+ * <li>strs[i] consists of only lowercase English letters.
+ */
+public class LongestCommonPrefix {
+    /**
+     * Find the longest common prefix string
+     * 
+     * @param strs an array of strings
+     * @return the longest common prefix
+     */
+    public String longestCommonPrefix(String[] strs) {
+        Arrays.sort(strs, (s, t) -> s.length() - t.length());
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < strs[0].length(); i++) {
+            char cur = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j++) {
+                if (cur != strs[j].charAt(i)) {
+                    return result.toString();
+                }
+            }
+            result.append(cur);
+        }
+
+        return result.toString();
+    }
+}
---
+++ src/test/java/com/leetcode/easy/LongestCommonPrefixTest.java
@@ -0,0 +1,33 @@
+/*
+ * LeetCode 14. Longest Common Prefix
+ * https://leetcode.com/problems/longest-common-prefix/description/
+ * 
+ * My repository with solutions
+ * https://github.com/egalli64/jex/
+ */
+package com.leetcode.easy;
+
+import java.util.stream.Stream;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class LongestCommonPrefixTest {
+    private static final LongestCommonPrefix instance = new LongestCommonPrefix();
+
+    static Stream<Arguments> provider() {
+        return Stream.of( //
+                Arguments.of(new String[] { "flower", "flow", "flight" }, "fl"), //
+                Arguments.of(new String[] { "dog", "racecar", "car" }, "") //
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provider")
+    void longestCommonPrefixExamples(String[] input, String expected) {
+        String actual = instance.longestCommonPrefix(input);
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+}
