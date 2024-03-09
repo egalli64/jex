096bdc0136da9d25fba698b977fa588583f1a5b4
Emanuele <egalli64@gmail.com>
2024-03-09 21:58:25 +0100

    simplify

--- src/test/java/com/leetcode/easy/LengthOfLastWordTest.java
+++ src/test/java/com/leetcode/easy/LengthOfLastWordTest.java
@@ -6,26 +6,15 @@
  */
 package com.leetcode.easy;
 
-import java.util.stream.Stream;
-
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class LengthOfLastWordTest {
     private static final LengthOfLastWord instance = new LengthOfLastWord();
 
-    private static final Stream<Arguments> provider() {
-        return Stream.of( //
-                Arguments.of("Hello World", 5), //
-                Arguments.of("   fly me   to   the moon  ", 4), //
-                Arguments.of("luffy is still joyboy", 6) //
-        );
-    }
-
     @ParameterizedTest
-    @MethodSource("provider")
+    @CsvSource({ "Hello World,5", "'   fly me   to   the moon  ', 4", "luffy is still joyboy, 6" })
     void examples(String input, int expected) {
         int actual = instance.lengthOfLastWord(input);
         Assertions.assertThat(actual).isEqualTo(expected);
