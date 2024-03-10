0c4c8f8325831fbfda4058acfe7f3a3505b15eb4
Emanuele <egalli64@gmail.com>
2024-03-10 11:53:17 +0100

    iterative solution added

--- src/main/java/com/leetcode/easy/BinaryTreeInorderTraversal.java
+++ src/main/java/com/leetcode/easy/BinaryTreeInorderTraversal.java
@@ -6,7 +6,9 @@
  */
 package com.leetcode.easy;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
 
 import com.leetcode.TreeNode;
@@ -15,7 +17,41 @@ import com.leetcode.TreeNode;
  * Convert a binary tree to list by inorder traversal
  */
 public class BinaryTreeInorderTraversal {
+    /**
+     * Iterative implementation for inorder traversal
+     * 
+     * Being the algorithm inherently recursive, so I do not expect any performance
+     * gain against the more elegant recursive implementation (see below)
+     * 
+     * @param root the tree root
+     * @return the list conversion by inorder traversal
+     */
     public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        TreeNode cur = root;
+        while (cur != null || !stack.isEmpty()) {
+            while (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            }
+
+            cur = stack.pop();
+            result.add(cur.val);
+
+            cur = cur.right;
+        }
+
+        return result;
+    }
+
+    /**
+     * Start the recursion from the user input
+     * 
+     * @param root the tree root
+     * @return the list conversion by inorder traversal
+     */
+    public List<Integer> recursiveInorderTraversal(TreeNode root) {
         List<Integer> result = new ArrayList<>();
         if (root != null) {
             traverse(root, result);
@@ -23,6 +59,12 @@ public class BinaryTreeInorderTraversal {
         return result;
     }
 
+    /**
+     * Recursion for inorder traversal
+     * 
+     * @param cur  the current node
+     * @param sink where to place the tree values
+     */
     private void traverse(TreeNode cur, List<Integer> sink) {
         if (cur.left != null) {
             traverse(cur.left, sink);
