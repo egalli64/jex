42d95f467f5ac8e22fa04bef725dbae362334f3b
Emanuele <egalli64@gmail.com>
2024-03-26 08:53:23 +0100

    robustness

--- src/test/java/com/leetcode/hard/FirstMissingPositiveTest.java
+++ src/test/java/com/leetcode/hard/FirstMissingPositiveTest.java
@@ -8,18 +8,38 @@ package com.leetcode.hard;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class FirstMissingPositiveTest {
     private static final FirstMissingPositive instance = new FirstMissingPositive();
 
-    @Test
-    void test1() {
-        assertThat(instance.firstMissingPositive(new int[] { 1, 2, 0 })).isEqualTo(3);
+    private static Stream<Arguments> provider() {
+        return Stream.of( //
+                Arguments.of(new int[] { 1, 2, 0 }, 3), //
+                Arguments.of(new int[] { 3, 4, -1, 1 }, 2), //
+                Arguments.of(new int[] { 7, 8, 9, 11, 12 }, 1) //
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provider")
+    void examples(int[] input, int expected) {
+        int actual = instance.firstMissingPositive(input);
+        assertThat(actual).isEqualTo(expected);
     }
 
     @Test
-    void test2() {
+    void minimalExtra() {
         assertThat(instance.firstMissingPositive(new int[] { 1 })).isEqualTo(2);
+    }
+
+    @Test
+    void minimalMissing() {
+        assertThat(instance.firstMissingPositive(new int[] { 0 })).isEqualTo(1);
     }
 }
