0cd8b1e18dcc3724257291b05218e743fedf805b
Emanuele <egalli64@gmail.com>
2024-07-11 08:03:36 +0200

    commented

--- src/main/java/com/leetcode/easy/CrawlerLogFolder.java
+++ src/main/java/com/leetcode/easy/CrawlerLogFolder.java
@@ -6,7 +6,21 @@
  */
 package com.leetcode.easy;
 
+/**
+ * Given a sequence of strings, each of them representing the argument for a
+ * change directory command, in the form (a) "../", (b) "./", or (c) "x/", where
+ * x is a valid string representing an existing folder. Assuming that all the cd
+ * commands are executed, return the minimum number of operations needed to go
+ * back to the main folder after the change folder operations.
+ */
 public class CrawlerLogFolder {
+    /**
+     * Keep track of the depth in the file system, looping on the logged commands,
+     * increasing for (c), decreasing for (a), doing nothing for (b)
+     * 
+     * @param logs the command log
+     * @return the final depth
+     */
     public int minOperations(String[] logs) {
         int result = 0;
 
