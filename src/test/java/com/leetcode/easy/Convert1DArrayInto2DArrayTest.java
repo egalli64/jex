2f959b660bee7227419d5f457989ff23767994c0
Emanuele <egalli64@gmail.com>
2025-02-04 09:42:40 +0100

    add header

--- src/test/java/com/leetcode/easy/Convert1DArrayInto2DArrayTest.java
+++ src/test/java/com/leetcode/easy/Convert1DArrayInto2DArrayTest.java
@@ -1,3 +1,9 @@
+/*
+ * 2022. Convert 1D Array Into 2D Array - https://leetcode.com/problems/convert-1d-array-into-2d-array/description/
+ * LeetCode Easy Problems - https://leetcode.com/problemset/?difficulty=EASY
+ * 
+ * My repository with solutions - https://github.com/egalli64/jex/
+ */
 package com.leetcode.easy;
 
 import java.util.stream.Stream;
