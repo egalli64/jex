d28c68d1bc493ac69e495d80f378c4905e9debe2
Emanuele <egalli64@gmail.com>
2025-08-14 09:50:32 +0200

    unused import

--- src/test/java/com/leetcode/easy/Largest3SameDigitNumberInStringTest.java
+++ src/test/java/com/leetcode/easy/Largest3SameDigitNumberInStringTest.java
@@ -10,13 +10,12 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class Largest3SameDigitNumberInStringTest {
     private static final Largest3SameDigitNumberInString instance = new Largest3SameDigitNumberInString();
 
     @ParameterizedTest
-    @CsvSource({ "6777133339,777", "2300019,000", "42352338,''" })
+    @CsvSource({"6777133339,777", "2300019,000", "42352338,''"})
     void examples(String input, String expected) {
         assertThat(instance.largestGoodInteger(input)).isEqualTo(expected);
     }
